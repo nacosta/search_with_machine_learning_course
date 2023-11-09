2798939e70b70a3c6107375c7a526e722c2ff005
Neftalí Acosta <neftaliacosta@gmail.com>
2023-11-09 20:07:11 +0000

    task 2

--- utilities/query.py
+++ utilities/query.py
@@ -17,6 +17,9 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
 
+import fasttext
+query_model = None
+
 # expects clicks and impressions to be in the row
 def create_prior_queries_from_group(
         click_group):  # total impressions isn't currently used, but it mayb worthwhile at some point
@@ -109,7 +112,7 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
                                         "minimum_should_match": "2<75%"
                                     }
                                 }
-                            }
+                            },
                         ],
                         "minimum_should_match": 1,
                         "filter": filters  #
@@ -186,12 +189,38 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
     return query_obj
 
 
-def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
+def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc", query_classifier: bool=False):
     #### W3: classify the query
     #### W3: create filters and boosts
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+
+    max_predicted_cats = 3
+    category_score_threshold = 0.5
+
+    global query_model
+    if query_classifier and query_model is None:
+        query_model = fasttext.load_model("/workspace/datasets/fasttext/model_week3.bin")
+
+    filters = None
+    if query_classifier:
+        predicted_categories, scores = query_model.predict(user_query, 3)
+        for p,s in zip(predicted_categories, scores):
+            print(f"{p} -> {s}")
+
+        filters = {'terms': {'categoryPathIds': []}}
+        total_score = 0
+        for i, score in enumerate(scores):
+            filters['terms']['categoryPathIds'].append(predicted_categories[i].replace('__label__', ''))
+            if (total_score:= total_score + score) >= category_score_threshold:
+                break
+
+        if total_score < category_score_threshold:
+            # discard filters if total_score below threshold
+            filters = None  
+
+    query_obj = create_query(user_query, click_prior_query=None, filters=filters, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
     logging.info(query_obj)
+    print(json.dumps(query_obj))
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
         hits = response['hits']['hits']
@@ -212,6 +241,8 @@ if __name__ == "__main__":
                          help='The OpenSearch port')
     general.add_argument('--user',
                          help='The OpenSearch admin.  If this is set, the program will prompt for password too. If not set, use default of admin/admin')
+    general.add_argument('--query_classifier', default=False, action='store_true',
+                         help='Filters using predicted category')
 
     args = parser.parse_args()
 
@@ -239,13 +270,14 @@ if __name__ == "__main__":
 
     )
     index_name = args.index
+    query_classifier = args.query_classifier
     query_prompt = "\nEnter your query (type 'Exit' to exit or hit ctrl-c):"
     print(query_prompt)
-    for line in fileinput.input():
-        query = line.rstrip()
+    while (query:= input()).lower() !=  "exit":
+        query = query.rstrip()
         if query == "Exit":
             break
-        search(client=opensearch, user_query=query, index=index_name)
+        search(client=opensearch, user_query=query, index=index_name, query_classifier=query_classifier)
 
         print(query_prompt)
 
