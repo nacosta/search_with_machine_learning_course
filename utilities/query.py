4faff200af80a21867b46aa3827d441ff91719c7
Neftalí Acosta <neftaliacosta@gmail.com>
2023-11-12 19:51:36 +0000

    week4

--- utilities/query.py
+++ utilities/query.py
@@ -17,6 +17,9 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
 
+from sentence_transformers import SentenceTransformer
+model = None
+
 # expects clicks and impressions to be in the row
 def create_prior_queries_from_group(
         click_group):  # total impressions isn't currently used, but it mayb worthwhile at some point
@@ -186,18 +189,41 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
     return query_obj
 
 
-def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
+def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc", use_vector="False"):
     #### W3: classify the query
     #### W3: create filters and boosts
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+    if use_vector:
+        query_obj = create_vector_query(user_query)
+    else:
+        query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
     logging.info(query_obj)
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
         hits = response['hits']['hits']
         print(json.dumps(response, indent=2))
 
+def create_vector_query(query):
+    global model 
+    if model is None:
+        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    embedding = model.encode(query)
+    query_obj = {
+        "size": 10,
+        "query": {
+            "knn": {
+                "embedding": {
+                    "vector": embedding.tolist(),
+                    "k": 10
+                }
+            }
+        }
+    }
+    print(json.dumps(query_obj))
 
+    return query_obj
+
+print(model)
 if __name__ == "__main__":
     host = 'localhost'
     port = 9200
@@ -212,6 +238,8 @@ if __name__ == "__main__":
                          help='The OpenSearch port')
     general.add_argument('--user',
                          help='The OpenSearch admin.  If this is set, the program will prompt for password too. If not set, use default of admin/admin')
+    general.add_argument('--vector', default=False, action='store_true',
+                         help='If this is set, use query embedding for retrieval')
 
     args = parser.parse_args()
 
@@ -241,10 +269,8 @@ if __name__ == "__main__":
     index_name = args.index
     query_prompt = "\nEnter your query (type 'Exit' to exit or hit ctrl-c):"
     print(query_prompt)
-    for line in fileinput.input():
-        query = line.rstrip()
-        if query == "Exit":
-            break
+    while (query:=input()) != "Exit":
+        query = query.rstrip()
         search(client=opensearch, user_query=query, index=index_name)
 
         print(query_prompt)
--- week4/utilities/index_products.py
+++ week4/utilities/index_products.py
@@ -21,9 +21,12 @@ logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
 
 # IMPLEMENT ME: import the sentence transformers module!
+from sentence_transformers import SentenceTransformer
 
 logger.info("Creating Model")
 # IMPLEMENT ME: instantiate the sentence transformer model!
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+print(model)
 
 # NOTE: this is not a complete list of fields.  If you wish to add more, put in the appropriate XPath expression.
 #TODO: is there a way to do this using XPath/XSL Functions so that we don't have to maintain a big list?
@@ -136,16 +139,26 @@ def index_file(file, index_name, reduced=False):
             continue
         if reduced and ('categoryPath' not in doc or 'Best Buy' not in doc['categoryPath'] or 'Movies & Music' in doc['categoryPath']):
             continue
-        docs.append({'_index': index_name, '_id':doc['sku'][0], '_source' : doc})
+        docs.append({'_index': index_name, '_id':doc['sku'][0], '_source' : doc, "name": doc['name']})
         #docs.append({'_index': index_name, '_source': doc})
         docs_indexed += 1
         if docs_indexed % 200 == 0:
             logger.info("Indexing")
+            names = [doc_["name"][0] for doc_ in docs]
+            embeddings = model.encode(names)
+            for i, doc_ in enumerate(docs):
+                doc_["_source"]["embedding"] = embeddings[i]
+
             bulk(client, docs, request_timeout=60)
             logger.info(f'{docs_indexed} documents indexed')
             docs = []
             names = []
+
     if len(docs) > 0:
+        names = [doc_["name"][0] for doc_ in docs]
+        embeddings = model.encode(names)
+        for i, doc_ in enumerate(docs):
+            doc_["_source"]["embedding"] = embeddings[i]
         bulk(client, docs, request_timeout=60)
         logger.info(f'{docs_indexed} documents indexed')
     return docs_indexed
